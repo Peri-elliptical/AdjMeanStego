952e26c1ef620ee6291234a9bf49db50b72d0f9c
Peri-Elliptical <len3rvz@gmail.com>
2026-04-05 20:11:52 +0530

    Debug

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const result = await response.json();
+console.log(result); 
 document.getElementById('output').innerText = result.status;
 document.getElementById('result_image').src = 'data:image/png;base64,' + result.image;
