5eb14f47e10f1e42d88d69b56e4800a25ff5dc7f
George Paul Paret <len3rvz@gmail.com>
2026-05-08 11:56:12 +0530

    Testing data flow

--- AdjMeanStego.c
+++ AdjMeanStego.c
@@ -9,5 +9,18 @@ uint8_t* create_buffer(int width, int height) {
 
 EMSCRIPTEN_KEEPALIVE
 void process_stego(uint8_t* img_data, int width, int height, const char* message, int msg_len) {
-    // C modifies img_data directly in place
+    // If the user typed a message, draw a 50x50 red square in the top-left corner
+    if (msg_len > 0) {
+        for (int y = 0; y < 50; y++) {
+            for (int x = 0; x < 50; x++) {
+                // Ensure we don't draw outside the image if it's smaller than 50x50
+                if (x < width && y < height) {
+                    int index = (y * width + x) * 3; // 3 bytes per pixel (RGB)
+                    img_data[index]     = 255; // Red
+                    img_data[index + 1] = 0;   // Green
+                    img_data[index + 2] = 0;   // Blue
+                }
+            }
+        }
+    }
 }
