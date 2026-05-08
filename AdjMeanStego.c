e5ab411307afdf8c942a750bb6d10176b45d12a0
George Paul Paret <len3rvz@gmail.com>
2026-05-08 23:14:09 +0530

    Testing width and height variables

--- AdjMeanStego.c
+++ AdjMeanStego.c
@@ -11,16 +11,14 @@ EMSCRIPTEN_KEEPALIVE
 void process_stego(uint8_t* img_data, int width, int height, const char* message, int msg_len) {
     // If the user typed a message, draw a 50x50 red square in the top-left corner
     if (msg_len > 0) {
-        for (int y = 0; y < 100; y++) {
-            for (int x = 0; x < 50; x++) {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < 1; x++) {
                 // Ensure we don't draw outside the image if it's smaller than 50x50
-                if (x < width && y < height) {
-                    int index = (y * width + x) * 4; // 4 bytes per pixel (RGBA)
-                    img_data[index]     = 255; // Red
-                    img_data[index + 1] = 0;   // Green
-                    img_data[index + 2] = 0;   // Blue
-                    img_data[index + 3] = 255; // Alpha
-                }
+                int index = (y * width + x) * 4; // 4 bytes per pixel (RGBA)
+                img_data[index]     = 255; // Red
+                img_data[index + 1] = 0;   // Green
+                img_data[index + 2] = 0;   // Blue
+                img_data[index + 3] = 255; // Alpha
             }
         }
     }
