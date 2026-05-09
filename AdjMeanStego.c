fff1fbfa5ba2c1f9b6ff07537d777ee15978992d
George Paul Paret <len3rvz@gmail.com>
2026-05-09 09:22:15 +0530

    Removing Red from an image

--- AdjMeanStego.c
+++ AdjMeanStego.c
@@ -11,14 +11,13 @@ EMSCRIPTEN_KEEPALIVE
 void process_stego(uint8_t* img_data, int width, int height, const char* message, int msg_len) {
     // If the user typed a message, draw a 50x50 red square in the top-left corner
     if (msg_len > 0) {
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < 1; x++) {
-                // Ensure we don't draw outside the image if it's smaller than 50x50
+        for (int y = 1; y < height - 1; y++) {
+            for (int x = 1; x < width - 1; x++) {
                 int index = (y * width + x) * 4; // 4 bytes per pixel (RGBA)
-                img_data[index]     = 255; // Red
-                img_data[index + 1] = 0;   // Green
-                img_data[index + 2] = 0;   // Blue
-                img_data[index + 3] = 255; // Alpha
+                img_data[index]     = 0; // Red
+                //img_data[index + 1] = 0;   // Green
+                //img_data[index + 2] = 0;   // Blue
+                //img_data[index + 3] = 255; // Alpha
             }
         }
     }
